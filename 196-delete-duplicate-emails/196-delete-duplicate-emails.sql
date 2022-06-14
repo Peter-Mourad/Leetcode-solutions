05d3e9b50adcd06b8b8d80c1305aba1b26f0544a
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2022-06-14 13:48:11 +0200

    Time: 1431 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,3 +1,2 @@
-DELETE p1 FROM person As p1
-INNER JOIN person AS p2
-ON p1.email = p2.email AND p1.id > p2.id
+DELETE p1 FROM person As p1, person AS p2
+WHERE p1.email = p2.email AND p1.id > p2.id
