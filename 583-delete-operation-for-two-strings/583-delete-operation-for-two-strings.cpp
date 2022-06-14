2319c5ad1fa78d152219ab4ceff2d7d853bcb6da
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2022-06-14 11:12:10 +0200

    Time: 73 ms (15.17%), Space: 7.7 MB (61.15%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,13 +3,15 @@ public:
     int dp[500][500];
     int solve(int i, int j, string &x, string &y){
         if(i==-1 || j==-1) return 0;
-        int &ret = dp[i][j];
-        if(~ret)return ret;
-        if(x[i]==y[j]) return ret = 2 + solve(i - 1, j - 1, x, y);
-        return ret = max(solve(i - 1, j, x, y), solve(i, j - 1, x, y));
+        if(~dp[i][j])return dp[i][j];
+        if(x[i]==y[j]) return dp[i][j] = 2 + solve(i - 1, j - 1, x, y);
+        return dp[i][j] = max(solve(i - 1, j, x, y), solve(i, j - 1, x, y));
     }
     int minDistance(string w1, string w2) {
-        memset(dp,-1,sizeof dp);
+        for(int i = 0;i < w1.size();i++){
+            for(int j = 0;j < w2.size();j++)
+                dp[i][j] = -1;
+        }
         return w1.size() + w2.size() - solve(w1.size() - 1, w2.size() - 1, w1, w2);
     }
 };
