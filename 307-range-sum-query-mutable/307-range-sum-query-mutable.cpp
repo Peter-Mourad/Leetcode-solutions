d15fe8ffda376f0d60af15977f8a5d0aac8abef3
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2022-07-31 09:56:56 +0200

    Time: 787 ms (29.51%), Space: 151.9 MB (58.98%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -0,0 +1,51 @@
+class NumArray {
+public:
+    
+    int t[120000], n;
+    void build(int i, int l, int r, vector<int> &x){
+        if(l == r) {
+            t[i] = x[l];return;
+        }
+        int mid = (l + r) / 2;
+        build(i * 2 + 1, l, mid, x);
+        build(i * 2 + 2, mid + 1, r, x);
+        t[i] = (t[i * 2 + 1] + t[i * 2 + 2]);
+    }
+    int query(int i,int l,int r,int s,int e){
+        if(l > e || r < s) return 0;
+        if(l >= s && r <= e)
+            return t[i];
+        int mid = (l + r) / 2;
+        return (query(i * 2 + 1, l, mid, s, e) + query(i * 2 + 2, mid + 1, r, s, e));
+    }
+    void update(int i,int l,int r,int pos,int val){
+        if(l == r)
+            t[i] = val;
+        else{
+            if(pos <= (l + r) / 2)
+                update(i * 2 + 1, l,(l + r) / 2,pos,val);
+            else update(i * 2 + 2,(l + r) / 2 + 1, r, pos, val);
+            t[i] = (t[i * 2 + 1] + t[i * 2 + 2]);
+        }
+    }
+    
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        build(0,0,nums.size() - 1,nums);
+    }
+    
+    void update(int index, int val) {
+        update(0,0,n - 1,index,val);
+    }
+    
+    int sumRange(int left, int right) {
+        return query(0,0,n - 1, left, right);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
