50fb11b839d15d6c3f307743e13a72aa17ac7e87
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2022-11-06 13:20:35 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (91.95%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -3,7 +3,7 @@ public:
     string orderlyQueue(string s, int k) {
         if(k==1){
             string ans = s;
-            for(int i = 1;i<=s.size();i++){
+            for(int i = 1;i<s.size();i++){
                 s+=s[0];
                 s.erase(0,1);
                 if(s < ans)
