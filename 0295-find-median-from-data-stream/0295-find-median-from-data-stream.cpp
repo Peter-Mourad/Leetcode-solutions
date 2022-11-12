b406e82fadc11e1f174c11dbdd705285ef24365f
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2022-11-12 10:52:23 +0200

    Time: 1202 ms (5.02%), Space: 134.7 MB (6.99%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,63 @@
+class MedianFinder {
+public:
+    int cnt = 0;
+    multiset<int> f, s;
+    MedianFinder() {
+        
+    }
+    // try to make first half holds (cnt + 1) / 2 elements
+    // and second half holds cnt / 2 elements
+    void addNum(int num) {
+        cnt++;
+        if(f.size() < (cnt + 1) / 2){
+            if(s.empty()){
+                f.insert(num);
+            }
+            else{
+                auto it = s.begin();
+                int num2 = *it;
+                if(num2 < num){
+                    s.erase(it);
+                    f.insert(num2);
+                    s.insert(num);
+                }
+                else 
+                    f.insert(num);
+            }
+        }
+        else{ 
+            auto it = f.end();
+            it--;
+            int num2 = *it;
+            if(num2 > num){
+                f.erase(it);
+                f.insert(num);
+                s.insert(num2);
+            }
+            else
+                s.insert(num);
+        }
+    }
+    
+    double findMedian() {
+        if(cnt & 1){
+            auto it = f.end();
+            it--;
+            return double(*it);
+        }
+        else{
+            auto it = f.end();
+            it--;
+            auto it2 = s.begin();
+            double ans = *it + *it2;
+            return ans / 2;
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
