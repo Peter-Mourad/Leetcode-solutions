cef414c2a973340469cc9747e5c788237a5e94bd
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2024-10-01 12:29:31 +0300

    [1581] [Time Beats: 55.75%] - LeetPush

---
+++ SQL 50 study plan/1581-Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
@@ -0,0 +1,5 @@
+select v.customer_id, count(v.visit_id) as count_no_trans from Visits as v
+left join Transactions as t
+on v.visit_id = t.visit_id
+where t.transaction_id is Null
+group by v.customer_id;
