9ddb7d638ae48f66fc444bd2be007c008b3bdfc9
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2024-10-19 12:41:52 +0300

    [1545] [Time Beats: 17.75%] [Memory Beats: 50.34%] - LeetPush

---
+++ 1545-Find-Kth-Bit-in-Nth-Binary-String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        /* 
+            the pattern in this problem is for every operation
+            we realize that the string is an inverted palindrome
+            i can identify the inverted palindrome as odd sized palindrome
+            with 1 in the middle and the first half is the inversion of
+            the second half
+        */
+
+        string ans = "0";
+        for(int i = 2;i <= n; i++){
+            ans += '1';
+            for(int j = ans.size() - 2; ~j ; j--)
+                ans += ((ans[j] - '0') ^ 1) + '0';
+        }
+        return ans[--k];
+    }
+};
