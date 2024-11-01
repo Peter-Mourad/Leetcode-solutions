4d559653f621e2619cf2503fe96c8077d5d8052c
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2024-11-01 16:52:51 +0200

    [111] [Time Beats: 82.07%] [Memory Beats: 95.08%] - LeetPush

---
+++ 111-Minimum-Depth-of-Binary-Tree.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(left==0 || (left > 0 && right > 0 && left > right)) left = right;
+        return left + 1;
+    }
+}
