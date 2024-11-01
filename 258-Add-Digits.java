82c890476688506db6ce8937ff17022d0df6d22f
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2024-11-01 17:08:31 +0200

    [258] [Time Beats: 51.95%] [Memory Beats: 33.61%] - LeetPush

---
+++ 258-Add-Digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while(String.valueOf(num).length() > 1){
+            int sum = 0;
+            while(num > 0){
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
