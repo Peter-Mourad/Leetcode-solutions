335d57a7a50153fa457518acc76932b7c152aa72
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2024-12-17 12:31:41 +0200

    Delete 1211-Queries-Quality-and-Percentage.sql

--- 1211-Queries-Quality-and-Percentage.sql
+++
@@ -1,4 +0,0 @@
-select query_name, round(avg(rating / position), 2) quality, 
-round(sum(case when rating < 3 then 1 else 0 end) * 100.0/ count(*), 2)  poor_query_percentage
-From Queries Q
-Group by query_name;
