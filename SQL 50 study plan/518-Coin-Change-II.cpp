29af3e82aad78d2181ec3b738c98998731d8415e
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2024-12-18 18:42:39 +0200

    [518] [Time Beats: 96.15%] [Memory Beats: 80.22%] - LeetPush

--- SQL 50 study plan/518-Coin-Change-II.cpp
+++ SQL 50 study plan/518-Coin-Change-II.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    vector<vector<int> > dp;
-    int solve(int i, int rem, vector<int> &coins){
-        if(!rem) return 1;
-        if(i==coins.size() || (i < coins.size() && coins[i] > rem))
-            return 0;
-        int &ret = dp[i][rem];
-        if(~ret) return ret;
-        ret = 0;
-        for(int x = 0;; x++){
-            if(x * coins[i] > rem) break;
-            ret += solve(i + 1, rem - x * coins[i], coins);
-        }
-        return ret;
-    }
     int change(int amount, vector<int>& coins) {
-        dp = vector<vector<int>> (coins.size(), vector<int> (amount + 1, -1));
         sort(coins.begin(), coins.end());
-        return solve(0, amount, coins);
+        vector<unsigned long long> dp(amount + 1);
+        dp[0] = 1;
+        
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++){
+                dp[j] += dp[j - coin];
+            }
+        }
+        
+        return dp[amount];
     }
 };
