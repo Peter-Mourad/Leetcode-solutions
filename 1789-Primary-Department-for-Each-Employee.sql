be44bf132daeb4c7d9bf736547f0793b3929be90
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2025-01-03 12:59:12 +0200

    Delete 1789-Primary-Department-for-Each-Employee.sql

--- 1789-Primary-Department-for-Each-Employee.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    e.employee_id,
-    COALESCE(ep.department_id, e.department_id) AS department_id
-FROM Employee e
-LEFT JOIN Employee ep 
-ON e.employee_id = ep.employee_id AND ep.primary_flag = 'Y'
-GROUP BY e.employee_id;
