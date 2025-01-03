ad3370f35027eb5b81b17f50bd85d5ee9a57c2b3
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2025-01-03 13:20:50 +0200

    [1164] [Time Beats: 22.16%] - LeetPush

---
+++ SQL 50 study plan/1164-Product-Price-at-a-Given-Date.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT product_id, 10 AS price FROM Products 
+WHERE product_id NOT IN(
+    SELECT DISTINCT product_id FROM Products 
+    WHERE change_date <='2019-08-16' 
+)
+UNION
+SELECT product_id, new_price price FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, max(change_date) AS date 
+    FROM Products 
+    WHERE change_date <='2019-08-16' 
+    GROUP BY product_id
+)
+GROUP BY product_id
+ORDER BY product_id;
