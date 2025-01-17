a9e51afb6f4b523de416225007dd6645036fdca8
Peter Mourad <98395157+Peter-Mourad@users.noreply.github.com>
2025-01-17 13:44:36 +0200

    [2683] [Time Beats: 29.44%] [Memory Beats: 19.16%] - LeetPush

--- 2683-Neighboring-Bitwise-XOR.cpp
+++ 2683-Neighboring-Bitwise-XOR.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n = derived.size();
-        vector<int> o(n);
-        for(int i = 1;i < n;i++){
+        vector<int> o(derived.size());
+        for(int i = 1;i < derived.size();i++){
             o[i] = o[i - 1] ^ derived[i - 1];
         }
-        return derived[n - 1]==o[0] ^ o[n - 1];
+        return derived.back()==o[0] ^ o.back();
     }
 };
